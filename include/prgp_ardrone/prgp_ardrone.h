fb337d91a108e1178f3444d39ab2e0555f298b82
Shengsong Yang <philyang@live.cn>
2015-08-05 15:31:00 +0100

    Formatted .cpp .h of prgp_ardrone

--- include/prgp_ardrone/prgp_ardrone.h
+++ include/prgp_ardrone/prgp_ardrone.h
@@ -1,36 +1,36 @@
 /*
-* Software License Agreement (BSD License)
-*
-* Copyright (c) 2015, University of York Robotics Laboratory (YRL).
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-*
-* * Redistributions of source code must retain the above copyright
-*   notice, this list of conditions and the following disclaimer.
-* * Redistributions in binary form must reproduce the above
-*   copyright notice, this list of conditions and the following
-*   disclaimer in the documentation and/or other materials provided
-*   with the distribution.
-* * Neither the name of the copyright holder nor the names of its
-*   contributors may be used to endorse or promote products derived
-*   from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2015, University of York Robotics Laboratory (YRL).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /**
  *  @file prgp_ardrone.h
@@ -88,7 +88,7 @@ private:
   //Publishers
   ros::Publisher landPub; /**< Publisher for sending the landing command directly to AR.Drone*/
   ros::Publisher takeoffPub; /**< Publisher for sending the takeoff command directly to AR.Drone */
-  ros::Publisher drone_pub;	/**< Publisher for sending flight command to AR.Drone by /tum_ardrone/com */
+  ros::Publisher drone_pub; /**< Publisher for sending flight command to AR.Drone by /tum_ardrone/com */
   ros::Publisher cmdPub; /**< Publisher for sending the command to Pi-Swarm by piswarm_com */
   ros::Publisher velPub; /**< Publisher for sending the command directly to AR.Drone by cmd_vel */
 
@@ -178,6 +178,5 @@ float currentPos_y;
 bool tag_detected = false;
 
 #endif//end of CLASS_STYLE
-
 
 #endif /* PRGP_ARDRONE_INCLUDE_PRGP_ARDRONE_H_ */
